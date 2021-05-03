2a0cb8049f99d1d39960f492232c5f486f313401
Jonas Fischer-Maeck <jonas@faibl.com>
2021-05-03 08:55:55 +0200

    feat: more modern loading indicator

--- src/components/_loadingIndictor.stories.js
+++ src/components/_loadingIndictor.stories.js
@@ -1,15 +1,13 @@
-import {DescriptionDecorator} from "../../.storybook/decorators";
-
 export default {
     title: 'Components/Loading/Loading Indicator',
 }
 
 export const Standard = () => `
-  <div class="sgbp-loadingIndicator"></div>
+  <div class="sgbp-loadingIndicator">Loading</div>
 `;
 
 export const Sizes = () => `
   <div class="sgbp-loadingIndicator sgbp-loadingIndicator--sizeS"></div>
-  <div class="sgbp-loadingIndicator sgbp-loadingIndicator--sizeM"></div>
-  <div class="sgbp-loadingIndicator sgbp-loadingIndicator--sizeL"></div>
+  <div class="sgbp-loadingIndicator sgbp-loadingIndicator--sizeM">Loading</div>
+  <div class="sgbp-loadingIndicator sgbp-loadingIndicator--sizeL">Loading</div>
 `;
