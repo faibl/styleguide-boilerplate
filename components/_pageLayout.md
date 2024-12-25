65c7ee788e60cebb54918330ecc18477d43e30c8
Jonas Fischer-Maeck <jonas@faibl.com>
2024-12-25 13:30:38 +0100

    Updates

--- components/_pageLayout.md
+++
@@ -1,3 +0,0 @@
-## Page Layout
-
-[Click here to open example layout](./layout.html)
--- styleguide.js
+++
@@ -1,7 +0,0 @@
-function setVHCssCustomProperty() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--sgbp-vh', `${vh}px`);
-}
-
-setVHCssCustomProperty();
-window.addEventListener('resize', setVHCssCustomProperty);
